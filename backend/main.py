6d0044f505aeac23bcdd985705efd1331a1ac1c2
Seba <sebamoisesdp@gmail.com>
2026-01-15 13:56:10 -0300

    Avances en backend de notas y ajustes de UI

--- backend/main.py
+++ backend/main.py
@@ -4,17 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=['http://localhost:5173/', 'http://localhost:5173'], allow_credentials=True, allow_methods=['*'])
-class User(BaseModel):
+class Task(BaseModel):
     name: str
-    surname: str
-    age: int
+    description: str
+    date: str
+    state: bool
+    created_at: str
+    update_at: str
 
 userslist = []
 
-@app.get('/')
+@app.get('/tasks')
 async def get() :
     return userslist
 
-@app.post('/users')
-async def users(user: User) :
-    userslist.append(user)
+@app.post('/tasks')
+async def users(task: Task) :
+    userslist.append(task)
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,41 @@
-let boton = document.getElementById('button');
-let input_name = document.getElementById('input_name');
-let input_surname = document.getElementById('input_surname');
-let input_age = document.getElementById('input_age');
-boton.addEventListener('click', async (e)=>{
-  e.preventDefault()
-  const user = {
-    name: input_name.value.trim(),
-    surname: input_surname.value.trim(),
-    age: Number(input_age.value.trim())
-  }
-  try{
-  const response = await fetch('http://127.0.0.1:8000/users', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(user)
-  })
-  if(!response.ok){
-    throw new Error(response.status)
-  }
-  const data = await response.json()
-  console.log('Informacion:' + data)
-}
-  catch (error){
-    console.error(error)
-  }
+let form = document.getElementById('form')
+let button_post = document.getElementById('button_post_note')
+document.addEventListener('DOMContentLoaded', ()=>{
+  form.addEventListener('submit', async ()=>{
+      let task_name = document.getElementById('input_task_name')
+      let task_description = document.getElementById('input_task_description')
+      let task_date = document.getElementById('input_task_date')
+      let task_checkbox = document.getElementById('input_checkbox')
+
+      const task = {
+        'name': task_name.value.trim(),
+        'description': task_description.value.trim(),
+        'date': task_date.value,
+        'state': task_checkbox.checked,
+        'created_at': 'null',
+        'update_at': 'null'
+      }
+      try{
+      let response = await fetch('http://127.0.0.1:8000/tasks',
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(task)
+      }
+      )
+      if(!response.ok){
+        throw new Error('Error HTTP')
+      }else{
+        alert('Mensaje enviado!')
+        task_name.value = null
+        task_description.value = null
+        task_date.value = null
+        task_checkbox.checked = false
+      }
+    }
+    catch (error){
+      console.error(error)
+    }
 })
+  })
+
